ffdf4b220dbeb887beffb902174772376f274003
arafat-213 <tai.arafat.at@gmail.com>
2021-01-23 12:58:40 +0530

    modified auth middleware to store user document

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken')
+const User = require('../models/user.model')
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
 	// Get token from req header
 	const token = req.header('x-auth-token')
 
@@ -15,11 +16,11 @@ module.exports = (req, res, next) => {
 		const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
 		//token is valid
-		req.user = decoded.user
-
+		req.user = await User.findById(decoded.id).select('-password')
 		// Middleware has done its job, onto the next one now
 		next()
 	} catch (error) {
+		console.error(error)
 		// Invalid token
 		return res.status(401).json({
 			error: 'Authentication token expired. Log in again.'
