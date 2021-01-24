d10b2e0624c9ab4a9031d1f756711e6faecb560a
arafat-213 <tai.arafat.at@gmail.com>
2021-01-24 18:17:11 +0530

    react-redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,33 @@
 import React, { Fragment, useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import axios from 'axios'
+// React components
 import Landing from './pages/Landing'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Restaurant from './pages/Restaurant'
+// Redux
+import { Provider } from 'react-redux'
+import store from './store'
 
 const App = () => {
-	useEffect(() => {
-		async function getData() {
-			const res = await axios.get('/api/restaurant/list')
-			console.table(res.data)
-		}
-		getData()
-	}, [])
 	return (
-		<Router>
-			<Switch>
-				<Route exact path='/' component={Landing} />
-				<Route exact path='/login' component={Login} />
-				<Route exact path='/signup' component={Signup} />
-				<Route exact path='/restaurant/:id' component={Restaurant} />
-			</Switch>
-			<Fragment>
-				<div>Hello World</div>
-			</Fragment>
-		</Router>
+		<Provider store={store}>
+			<Router>
+				<Switch>
+					<Route exact path='/' component={Landing} />
+					<Route exact path='/login' component={Login} />
+					<Route exact path='/signup' component={Signup} />
+					<Route
+						exact
+						path='/restaurant/:id'
+						component={Restaurant}
+					/>
+				</Switch>
+				<Fragment>
+					<div>Hello World</div>
+				</Fragment>
+			</Router>
+		</Provider>
 	)
 }
 
