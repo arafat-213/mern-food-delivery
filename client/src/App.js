99c7dae76d22f1eef5fa32454f32c6657faef698
arafat-213 <tai.arafat.at@gmail.com>
2021-01-24 13:19:42 +0530

    client is now connected to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
+import axios from 'axios'
 
-const App = () => (
-	<Fragment>
-		<div>Hello World</div>
-	</Fragment>
-)
+const App = () => {
+	useEffect(() => {
+		async function getData() {
+			const res = await axios.get('/api/restaurant/list')
+			console.table(res.data)
+		}
+		getData()
+	}, [])
+	return (
+		<Fragment>
+			<div>Hello World</div>
+		</Fragment>
+	)
+}
 
 export default App
