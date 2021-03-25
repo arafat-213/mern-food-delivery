ba6ab8d5753d0d9557445a28a46cbed3e4bdafb2
arafat-213 <tai.arafat.at@gmail.com>
2021-03-25 20:35:46 +0530

    added order reducer on root reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import { auth } from './auth.reducer'
 import { restaurant } from './restaurant.reducer'
-import {cart} from './cart.reducer'
+import { cart } from './cart.reducer'
+import { order } from './order.reducer'
 
-export default combineReducers({ auth, restaurant, cart })
+export default combineReducers({ auth, restaurant, cart, order })
