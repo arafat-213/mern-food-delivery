b95c8338164a5b2be27ddb7723b4e7564473fdef
arafat-213 <tai.arafat.at@gmail.com>
2021-04-13 22:22:56 +0530

    added waves on landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
+// React-bootstrap
+import { Container, Row, Col } from 'react-bootstrap'
+
+// CSS
+import './Landing/Landing.css'
+import Waves from './Landing/waves.svg'
+
 const Landing = () => {
-	return <div>Landing page</div>
+	return (
+		<Container className='landing d-flex flex-column justify-content-between h-100 px-0'>
+			<Row className='mx-auto'>
+				<Col className=''>
+					<h1 className='text-dark font-weight-bold'>
+						Welcome to FoodZilla
+					</h1>
+					<h3>One place solution for all your delicious cravings</h3>
+				</Col>
+			</Row>
+
+			<img src={Waves} alt='waves' className='waves' />
+		</Container>
+	)
 }
 
 export default Landing
