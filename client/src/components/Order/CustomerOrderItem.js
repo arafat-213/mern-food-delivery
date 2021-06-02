0acfd07d9e172d3b06b0a36fff4f8d0d89365434
arafat-213 <tai.arafat.at@gmail.com>
2021-06-02 17:17:46 +0530

    added styles on customer orders page

--- client/src/components/Order/CustomerOrderItem.js
+++ client/src/components/Order/CustomerOrderItem.js
@@ -1,40 +1,94 @@
 import React from 'react'
+import moment from 'moment'
 
 // Bootstrap
 import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 const CustomerOrderItem = ({
 	order: {
 		orderStatus,
-		restaurant: { name, cuisine },
+		restaurant: { name },
 		totalAmount,
 		orderContent,
 		createdAt
 	}
 }) => {
+	const getOrderStautsBadge = orderStatus => {
+		switch (orderStatus) {
+			case 'W':
+				return (
+					<Button variant='outline-warning' disabled>
+						Waiting for restuarant to confirm your order!
+					</Button>
+				)
+			case 'A':
+				return (
+					<Button variant='outline-success' disabled>
+						Restaurant has accepted your order!
+					</Button>
+				)
+
+			case 'R':
+				return (
+					<Button variant='outline-danger' disabled>
+						Restaurant has rejected your order!
+					</Button>
+				)
+			case 'I':
+				return (
+					<Button variant='outline-primary' disabled>
+						Your order is being prepared!
+					</Button>
+				)
+			case 'O':
+				return (
+					<Button disabled variant='outline-info'>
+						Your order is on the way!
+					</Button>
+				)
+			case 'D':
+				return (
+					<Button disabled variant='outline-secondary'>
+						Your order has been delivered
+					</Button>
+				)
+			default:
+				return (
+					<Button disabled variant='outline-danger'>
+						Order status unavailable.
+					</Button>
+				)
+		}
+	}
+
 	return (
 		<Card>
 			<Card.Title>
-				<div>
-					<h3>{name}</h3>
-					<h5>{cuisine.toString()}</h5>
-					<h3>Total amount : {`${totalAmount} rs`}</h3>
+				<div className='d-flex justify-content-between'>
+					<h3 className='p-1'>{name}</h3>
+					{getOrderStautsBadge(orderStatus)}
 				</div>
 			</Card.Title>
-			<Card.Body>
-				<p>Order status: {orderStatus}</p>
-				<p>items:</p>
+			<Card.Body className='p-2'>
 				{orderContent.map(item => (
 					<div>
-						<p>
-							{item.itemName} : {item.itemPrice} rs
+						<p className='py-0 my-0'>
+							{item.itemName} : {item.itemPrice.toFixed(2)}{' '}
+							&#x20B9;
 						</p>
 					</div>
 				))}
+				<h5>Total amount : {`${totalAmount.toFixed(2)}`} &#x20B9;</h5>
 			</Card.Body>
-			<Card.Footer>
-				<p>Ordered on {createdAt}</p>
-				<p>Repeat order</p>
+			<Card.Footer className='border-top-0 p-2'>
+				<p className='mb-1'>
+					{' '}
+					Ordered on {moment(createdAt).format('LLL')}
+				</p>
+				<p className='mb-0'>
+					<i class='fas fa-redo'></i> Repeat order
+				</p>
 			</Card.Footer>
 		</Card>
 	)
